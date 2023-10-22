49640e3c68b3ae265fee2f7800ee10b0d1b9236e
Eric Shields <shieldse01@yahoo.com>
2023-10-21 20:30:22 -0500

    Sentiment Analysis

--- streamlit_frontend/app.py
+++ streamlit_frontend/app.py
@@ -1,11 +1,25 @@
-from flask import Flask, g
+from flask import Flask, g, request
 from flask_cors import CORS
 import sqlite3
+import pickle
+from sklearn.neural_network import MLPClassifier
+import openai
+from dotenv import load_dotenv
+import os
+from config import prompts
 
 app = Flask(__name__)
 CORS(app)
-app.config["DATABASE"] = "./backend_database.db"
 
+app.config["DATABASE"] = "./backend_database.db"
+app.config["MODELPATH"] = "../model_code/finalized_model.sav"
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAIAPIKEY")
+openai.organization = os.getenv("OPENAIORGANIZATION")
+print(openai.Model.list())
+
+loaded_model = pickle.load(open(app.config["MODELPATH"], 'rb'))
 
 def get_db():
     db = getattr(g, "_database", None)
@@ -35,3 +49,36 @@ def close_connection(exception):
 def hello_world():
     get_db()
     return "Hello, World!"
+
+@app.route("/postResponse/<id>", methods=["POST"])
+def postResponse(id):
+    form = request.form
+    question = form["question"]
+    answer = form["answer"]
+
+    return sentimentAnalysis(question, answer)
+
+@app.route("/serveModel")
+def serve_model():
+    data = [0.854096, 0.985810, 0, 0, 0]
+
+    prediction = loaded_model.predict([data])
+
+    return str(prediction[0])
+
+def sentimentAnalysis(question, answer):
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "Give only a number response from 1-100 on how positive you would rate the response to the question: " + question},
+            {"role": "user", "content": answer},
+        ],
+        max_tokens=300,
+        temperature=0.7,
+        top_p=1,
+    )
+
+    return str(int(completion['choices'][0]['message']['content']) / 100)
+
+    
